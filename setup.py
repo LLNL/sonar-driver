3d3e0089dfd72aaa03626969a714f267a733da65
Alfredo Adolfo Gimenez <gimenez1@llnl.gov>
2018-07-10 15:53:20 -0700

    Added install requirements to setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,18 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-        name='sonar-driver',
-        version='0.1',
-        description='Scripts for driving Sonar',
-        long_description=readme,
-        author='Alfredo Gimenez',
-        author_email='gimenez1@llnl.gov',
-        url='https://lc.llnl.gov/bitbucket/projects/SON/repos/sonar-driver',
-        license=license,
-        packages=find_packages(exclude=('tests', 'docs'))
-        )
+    name='sonar-driver',
+    version='0.1',
+    description='Scripts for driving Sonar',
+    long_description=readme,
+    author='Alfredo Gimenez',
+    author_email='gimenez1@llnl.gov',
+    url='https://lc.llnl.gov/bitbucket/projects/SON/repos/sonar-driver',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'cassandra-driver',
+        'requests',
+        'pygments'
+    ]
+)
