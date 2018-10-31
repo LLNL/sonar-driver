4f677afc566595b289e896cd234025203010bbad
Alfredo Adolfo Gimenez <gimenez1@llnl.gov>
2018-10-31 15:22:21 -0700

    Get kafka rest info from env if possible

--- bin/setup_env.sh
+++ bin/setup_env.sh
@@ -5,17 +5,19 @@ CLUSTER=${HOSTNAME//[0-9]/}
 if [[ "${CLUSTER}" == rzsonar ]]
 then
     export KAFKA_REST_URL=http://rzsonar8
-    export KAFKA_REST_PORT=8083
+    export KAFKA_CONNECT_PORT=8083
+    export KAFKA_SCHEMA_REGISTRY_PORT=8085
     export ZOOKEEPER_HOST=rzsonar8
     export ZOOKEEPER_PORT=2181
     export CQLSH_HOST=rzsonar8
     export CQLSH_PORT=9042
 elif [[ "${CLUSTER}" == sonar ]]
 then
-    export KAFKA_REST_URL=http://sonar11
-    export KAFKA_REST_PORT=8083
-    export ZOOKEEPER_HOST=sonar11
+    export KAFKA_REST_URL=http://sonar1
+    export KAFKA_CONNECT_PORT=8083
+    export KAFKA_SCHEMA_REGISTRY_PORT=8085
+    export ZOOKEEPER_HOST=sonar1
     export ZOOKEEPER_PORT=2181
-    export CQLSH_HOST=sonar11
+    export CQLSH_HOST=sonar1
     export CQLSH_PORT=9042
 fi
